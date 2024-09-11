aa442a71691a621984a676952f39a8142c40adb3
JAY <jaysharma2913@gmail.com>
2024-09-11 11:31:27 +0530

    Python-Class-1

---
+++ myModules.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "6e96c01f-f562-43b8-888b-30ad1715ee58",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#0 import PythonClassroom"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "f684c8ab-51ea-4dc0-9b37-5014cac62a3c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
